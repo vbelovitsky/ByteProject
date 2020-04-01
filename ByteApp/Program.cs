d7f637bdc9d9ea4023329a02dae0cf946b909aec
ruzik <ruslan.g2002@gmail.com>
2020-04-01 12:05:37 +0300

    Menu creation optimisation

--- ByteApp/Program.cs
+++ ByteApp/Program.cs
@@ -5,288 +5,291 @@ using ByteLibrary;
 
 namespace ByteApp
 {
-	class Program
-	{
-		private static string FILE_PATH = "../../../t.dat";
-		private static string FILE_PATH_2 = "../../../Numbers.dat";
+    class Program
+    {
+        private static string FILE_PATH = "../../../t.dat";
+        private static string FILE_PATH_2 = "../../../Numbers.dat";
 
-		private static Random rnd = new Random();
+        private static Random rnd = new Random();
 
-		private static int MENU_ITEMS = 5; // +1
+        private static int MENU_ITEMS = 5; // +1
 
-		/// <summary>
-		/// Запускает меню
-		/// </summary>
-		/// <param name="position"></param>
-		static void RunMenu(int position)
-		{
-			ConsoleKey consoleKey;
-			do
-			{
-				Console.Clear();
+        /// <summary>
+        /// Запускает меню
+        /// </summary>
+        /// <param name="position"></param>
+        static void RunMenu(int position)
+        {
+            ConsoleKey consoleKey;
+            do
+            {
+                Console.Clear();
 
-				DrawMenu(position);
-				
-				consoleKey = Console.ReadKey().Key;
-				switch (consoleKey)
-				{
-					case ConsoleKey.UpArrow:
-						position = position - 1 < 0 ? 0 : position - 1;
-						RunMenu(position);
-						break;
-					case ConsoleKey.DownArrow:
-						position = position + 1 > MENU_ITEMS ? MENU_ITEMS : position + 1;
-						RunMenu(position);
-						break;
+                DrawMenu(position);
 
-					case ConsoleKey.Enter:
-						switch (position)
-						{
-							case 0:
-								WriteToFile();
-								break;
-							case 1:
-								ReadFromFile();
-								break;
-							case 2:
-								IncreaseNumbersInFile();
-								break;
-							case 3:
-								ReadReversedNumbers();
-								break;
-							case 4:
-								FillRandomNumbers();
-								break;
-							case 5:
-								ChangeRandomNumbers();
-								break;
-						}
-						break;
-				}
+                consoleKey = Console.ReadKey().Key;
+                switch (consoleKey)
+                {
+                    case ConsoleKey.UpArrow:
+                        position = position - 1 < 0 ? 0 : position - 1;
+                        RunMenu(position);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        position = position + 1 > MENU_ITEMS ? MENU_ITEMS : position + 1;
+                        RunMenu(position);
+                        break;
 
-			}
-			while (consoleKey != ConsoleKey.Escape);
-		}
+                    case ConsoleKey.Enter:
+                        switch (position)
+                        {
+                            case 0:
+                                WriteToFile();
+                                break;
+                            case 1:
+                                ReadFromFile();
+                                break;
+                            case 2:
+                                IncreaseNumbersInFile();
+                                break;
+                            case 3:
+                                ReadReversedNumbers();
+                                break;
+                            case 4:
+                                FillRandomNumbers();
+                                break;
+                            case 5:
+                                ChangeRandomNumbers();
+                                break;
+                        }
 
-		/// <summary>
-		/// Рисует меню
-		/// </summary>
-		/// <param name="position">Позиция курсора</param>
-		static void DrawMenu(int position)
-		{
-			StringBuilder menu = new StringBuilder();
-			menu.Append("Choose action:").Append(Environment.NewLine);
-			menu.Append(position == 0 ? "[x]" : "[ ]").Append(" Write numbers to file.").Append(Environment.NewLine);
-			menu.Append(position == 1 ? "[x]" : "[ ]").Append(" Read numbers from file.").Append(Environment.NewLine);
-			menu.Append(position == 2 ? "[x]" : "[ ]").Append(" Increase numbers in file.").Append(Environment.NewLine);
-			menu.Append(position == 3 ? "[x]" : "[ ]").Append(" Read reversed numbers.").Append(Environment.NewLine);
-			menu.Append(position == 4 ? "[x]" : "[ ]").Append(" Fill random numbers.").Append(Environment.NewLine);
-			menu.Append(position == 5 ? "[x]" : "[ ]").Append(" Change random numbers.").Append(Environment.NewLine);
+                        break;
+                }
+            } while (consoleKey != ConsoleKey.Escape);
+        }
 
-			menu.Append(Environment.NewLine).Append("Press Esc button to exit from menu: ");
+        private static string[] menuItems =
+        {
+            "Write numbers to file.",
+            "Read numbers from file.",
+            "Increase numbers in file.",
+            "Read reversed numbers.",
+            "Fill random numbers.",
+            "Change random numbers."
+        };
 
-			Console.WriteLine(menu.ToString());
-		}
+        /// <summary>
+        /// Рисует меню
+        /// </summary>
+        /// <param name="position">Позиция курсора</param>
+        static void DrawMenu(int position)
+        {
+            StringBuilder menu = new StringBuilder();
+            menu.Append("Choose action:").Append(Environment.NewLine);
+            for (int i = 0; i < MENU_ITEMS + 1; i++)
+                menu.Append(position == i ? "[x]" : "[ ]").Append(" ").Append(menuItems[i]).Append(Environment.NewLine);
 
-		/// <summary>
-		/// Записывает в файл последовательность из пяти чисел
-		/// </summary>
-		static void WriteToFile()
-		{
-			Console.Clear();
-			FileStream fileStream = new FileStream(FILE_PATH, FileMode.Create);
-			CustomByteWriter writer = new CustomByteWriter(fileStream);
+            menu.Append(Environment.NewLine).Append("Press Esc button to exit from menu: ");
 
-			int[] arr = new int[5];
-			for(int i = 0; i < arr.Length; i++)
-			{
-				arr[i] = i + 1;
-			}
+            Console.WriteLine(menu.ToString());
+        }
 
-			writer.WriteArray(arr);
-			writer.Close();
-			Console.WriteLine($"Numbers are written to a file: {string.Join(" ", arr)}");
-			Console.Write("Press any button to return to menu: ");
-			Console.ReadKey();
-		}
+        /// <summary>
+        /// Записывает в файл последовательность из пяти чисел
+        /// </summary>
+        static void WriteToFile()
+        {
+            Console.Clear();
+            FileStream fileStream = new FileStream(FILE_PATH, FileMode.Create);
+            CustomByteWriter writer = new CustomByteWriter(fileStream);
 
-		static void ReadFromFile()
-		{
-			Console.Clear();
-			FileStream fileStream = new FileStream(FILE_PATH, FileMode.Open);
-			CustomByteReader reader = new CustomByteReader(fileStream);
+            int[] arr = new int[5];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i + 1;
+            }
 
-			int[] arr = reader.ReadArray<int>();
+            writer.WriteArray(arr);
+            writer.Close();
+            Console.WriteLine($"Numbers are written to a file: {string.Join(" ", arr)}");
+            Console.Write("Press any button to return to menu: ");
+            Console.ReadKey();
+        }
 
-			reader.Close();
-			Console.WriteLine(string.Join(" ", arr));
-			Console.Write("Press any button to return to menu: ");
-			Console.ReadKey();
-		}
+        static void ReadFromFile()
+        {
+            Console.Clear();
+            FileStream fileStream = new FileStream(FILE_PATH, FileMode.Open);
+            CustomByteReader reader = new CustomByteReader(fileStream);
 
-		/// <summary>
-		/// Умножает числа на введенный множитель
-		/// </summary>
-		static void IncreaseNumbersInFile()
-		{
-			Console.Clear();
-			FileStream fileStream = new FileStream(FILE_PATH, FileMode.Open);
-			CustomByteReader reader = new CustomByteReader(fileStream);
-			int[] arr = reader.ReadArray<int>();
-			reader.Close();
-			Console.WriteLine($"Initial numbers: {string.Join(" ", arr)}");
+            int[] arr = reader.ReadArray<int>();
 
-			
-			fileStream = new FileStream(FILE_PATH, FileMode.Create);
-			CustomByteWriter writer = new CustomByteWriter(fileStream);
+            reader.Close();
+            Console.WriteLine(string.Join(" ", arr));
+            Console.Write("Press any button to return to menu: ");
+            Console.ReadKey();
+        }
 
-			int multiplier = ReadInt("multiplier", -10, 10);
-			for (int i = 0; i < arr.Length; i++)
-			{
-				arr[i] *= multiplier;
-			}
-
-			Console.WriteLine($"Increased numbers: {string.Join(" ", arr)}");
-			writer.WriteArray(arr);
-			writer.Close();
-
-			Console.Write("Press any button to return to menu: ");
-			Console.ReadKey();
-		}
-
-		/// <summary>
-		/// Считывает числа из файла в обратном порядке
-		/// </summary>
-		static void ReadReversedNumbers()
-		{
-			Console.Clear();
-			FileStream fileStream = new FileStream(FILE_PATH, FileMode.Open);
-			CustomByteReader reader = new CustomByteReader(fileStream);
-
-			int[] arr = reader.ReadReversedArray<int>();
-
-			reader.Close();
-			Console.WriteLine(string.Join(" ", arr));
-			Console.Write("Press any button to return to menu: ");
-			Console.ReadKey();
-		}
-
-		/// <summary>
-		/// Создает 10 рандомных чисел и записывает в файл
-		/// </summary>
-		static void FillRandomNumbers()
-		{
-			Console.Clear();
-			FileStream fileStream = new FileStream(FILE_PATH_2, FileMode.Create);
-			CustomByteWriter writer = new CustomByteWriter(fileStream);
-
-			int[] arr = new int[10];
-			for (int i = 0; i < arr.Length; i++)
-			{
-				arr[i] = rnd.Next(1, 101);
-			}
-
-			writer.WriteArray(arr);
-			writer.Close();
-			Console.WriteLine($"Random numbers are written to a file: {string.Join(" ", arr)}");
-			Console.Write("Press any button to return to menu: ");
-			Console.ReadKey();
-		}
-
-		/// <summary>
-		/// Позволяет пользователю менять числа
-		/// </summary>
-		static void ChangeRandomNumbers()
-		{
-
-			do
-			{
-				Console.Clear();
-
-				FileStream fileStream = new FileStream(FILE_PATH_2, FileMode.Open);
-				CustomByteReader reader = new CustomByteReader(fileStream);
-				int[] arr = reader.ReadArray<int>();
-				Console.WriteLine(string.Join(" ", arr));
-				reader.Close();
-
-				int userNum = ReadInt("number", 1, 100);
-				int[] minDist = new int[arr.Length];
-				for(int i = 0; i < arr.Length; i++)
-				{
-					minDist[i] = Math.Abs(arr[i] - userNum);
-				}
-				int valueMinDist = int.MaxValue;
-				int valueMinDistIndex = -1;
-				for (int i = 0; i < minDist.Length; i++)
-				{
-					if (minDist[i] < valueMinDist)
-					{
-						valueMinDistIndex = i;
-						valueMinDist = minDist[i];
-					}
-				}
-
-				arr[valueMinDistIndex] = userNum;
-
-				// Write changes
-				fileStream = new FileStream(FILE_PATH_2, FileMode.Create);
-				CustomByteWriter writer = new CustomByteWriter(fileStream);
-				writer.WriteArray(arr);
-				writer.Close();
+        /// <summary>
+        /// Умножает числа на введенный множитель
+        /// </summary>
+        static void IncreaseNumbersInFile()
+        {
+            Console.Clear();
+            FileStream fileStream = new FileStream(FILE_PATH, FileMode.Open);
+            CustomByteReader reader = new CustomByteReader(fileStream);
+            int[] arr = reader.ReadArray<int>();
+            reader.Close();
+            Console.WriteLine($"Initial numbers: {string.Join(" ", arr)}");
 
 
-				// Read again
-				fileStream = new FileStream(FILE_PATH_2, FileMode.Open);
-				reader = new CustomByteReader(fileStream);
-				arr = reader.ReadArray<int>();
-				Console.WriteLine($"Modifyed nums: {string.Join(" ", arr)}");
-				reader.Close();
+            fileStream = new FileStream(FILE_PATH, FileMode.Create);
+            CustomByteWriter writer = new CustomByteWriter(fileStream);
+
+            int multiplier = ReadInt("multiplier", -10, 10);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] *= multiplier;
+            }
+
+            Console.WriteLine($"Increased numbers: {string.Join(" ", arr)}");
+            writer.WriteArray(arr);
+            writer.Close();
+
+            Console.Write("Press any button to return to menu: ");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Считывает числа из файла в обратном порядке
+        /// </summary>
+        static void ReadReversedNumbers()
+        {
+            Console.Clear();
+            FileStream fileStream = new FileStream(FILE_PATH, FileMode.Open);
+            CustomByteReader reader = new CustomByteReader(fileStream);
+
+            int[] arr = reader.ReadReversedArray<int>();
+
+            reader.Close();
+            Console.WriteLine(string.Join(" ", arr));
+            Console.Write("Press any button to return to menu: ");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Создает 10 рандомных чисел и записывает в файл
+        /// </summary>
+        static void FillRandomNumbers()
+        {
+            Console.Clear();
+            FileStream fileStream = new FileStream(FILE_PATH_2, FileMode.Create);
+            CustomByteWriter writer = new CustomByteWriter(fileStream);
+
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(1, 101);
+            }
+
+            writer.WriteArray(arr);
+            writer.Close();
+            Console.WriteLine($"Random numbers are written to a file: {string.Join(" ", arr)}");
+            Console.Write("Press any button to return to menu: ");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Позволяет пользователю менять числа
+        /// </summary>
+        static void ChangeRandomNumbers()
+        {
+            do
+            {
+                Console.Clear();
+
+                FileStream fileStream = new FileStream(FILE_PATH_2, FileMode.Open);
+                CustomByteReader reader = new CustomByteReader(fileStream);
+                int[] arr = reader.ReadArray<int>();
+                Console.WriteLine(string.Join(" ", arr));
+                reader.Close();
+
+                int userNum = ReadInt("number", 1, 100);
+                int[] minDist = new int[arr.Length];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    minDist[i] = Math.Abs(arr[i] - userNum);
+                }
+
+                int valueMinDist = int.MaxValue;
+                int valueMinDistIndex = -1;
+                for (int i = 0; i < minDist.Length; i++)
+                {
+                    if (minDist[i] < valueMinDist)
+                    {
+                        valueMinDistIndex = i;
+                        valueMinDist = minDist[i];
+                    }
+                }
+
+                arr[valueMinDistIndex] = userNum;
+
+                // Write changes
+                fileStream = new FileStream(FILE_PATH_2, FileMode.Create);
+                CustomByteWriter writer = new CustomByteWriter(fileStream);
+                writer.WriteArray(arr);
+                writer.Close();
 
 
-				Console.WriteLine("Press Esc button to exit modifying: ");
-			}
-			while (Console.ReadKey().Key != ConsoleKey.Escape);
+                // Read again
+                fileStream = new FileStream(FILE_PATH_2, FileMode.Open);
+                reader = new CustomByteReader(fileStream);
+                arr = reader.ReadArray<int>();
+                Console.WriteLine($"Modifyed nums: {string.Join(" ", arr)}");
+                reader.Close();
 
 
-			Console.Write("Press any button to return to menu: ");
-			Console.ReadKey();
-		}
-
-		/// <summary>
-		/// Метод для корректного ввода целого числа
-		/// </summary>
-		/// <param name="name">Имя числа</param>
-		/// <param name="min">минимальное значение</param>
-		/// <param name="max">максимальное значение</param>
-		/// <returns></returns>
-		static int ReadInt(string name, int min, int max)
-		{
-			Console.WriteLine($"Input {name} in interval [{min}, {max}]:");
-			int num;
-			while (!(int.TryParse(Console.ReadLine(), out num) && num >= min && num <= max))
-			{
-				Console.WriteLine("Try again:");
-			}
-			return num;
-		}
+                Console.WriteLine("Press Esc button to exit modifying: ");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
 
 
-		
-		static void Main(string[] args)
-		{
+            Console.Write("Press any button to return to menu: ");
+            Console.ReadKey();
+        }
 
-			do
-			{
-				Console.Clear();
+        /// <summary>
+        /// Метод для корректного ввода целого числа
+        /// </summary>
+        /// <param name="name">Имя числа</param>
+        /// <param name="min">минимальное значение</param>
+        /// <param name="max">максимальное значение</param>
+        /// <returns></returns>
+        static int ReadInt(string name, int min, int max)
+        {
+            Console.WriteLine($"Input {name} in interval [{min}, {max}]:");
+            int num;
+            while (!(int.TryParse(Console.ReadLine(), out num) && num >= min && num <= max))
+            {
+                Console.WriteLine("Try again:");
+            }
 
-				RunMenu(0);
+            return num;
+        }
 
-				Console.Clear();
+        /// <summary>
+        /// Точка входа программы
+        /// </summary>
+        static void Main()
+        {
+            do
+            {
+                Console.Clear();
 
-				Console.Write("Press Esc button to exit: ");
-			}
-			while (Console.ReadKey().Key != ConsoleKey.Escape);
+                RunMenu(0);
 
-		}
-	}
+                Console.Clear();
+
+                Console.Write("Press Esc button to exit: ");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
 }
